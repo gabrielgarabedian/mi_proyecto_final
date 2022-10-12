dfe08f0ea26a4d2cf0a70485290d8bd6ae6fda0a
Gabriel Garabedian <gabrielgarabedian@hotmail.com>
2022-10-11 23:33:39 -0300

    modificacion 1

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html", {"nombre":"Gabriel"})
+
+def index2(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html", {"nombre":nombre, "apellido": apellido})
--- project/urls.py
+++ project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import index
+from ejemplo.views import index, index2
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', index),
+    path('saludar/<nombre>/<apellido>/', index2),
 ]
