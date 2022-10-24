65c53d8c7918778637c5d950109b3c23664501d0
Gabriel Garabedian <gabrielgarabedian@hotmail.com>
2022-10-23 21:48:44 -0300

    Se cargaron titulos y se crearon usuarios

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from traitlets import default
 
-class Configuarcion(model.Model):
-    nombre_blog
+class Configuracion(models.Model):
+   nombre_blog = models.CharField(max_length=14)
+   construido_por = models.CharField(max_length=30)
+   titulo_principal= models.CharField(max_length=30, default='')
+   subtitulo_principal= models.CharField(max_length=60, default='')
+   
